70eb1cbe1b8e5af3a03182d76d0a005145778af4
eperesite <127439584+eperesite@users.noreply.github.com>
2023-10-24 18:20:55 +0300

    Update LoginManager.java

--- project/src/Engine/Authentication/LoginManager.java
+++ project/src/Engine/Authentication/LoginManager.java
@@ -1,4 +1,23 @@
 package Engine.Authentication;
 
 public class LoginManager {
+
+    private String username;
+    private String password;
+
+   public Customer( String username, String password) {
+        this.username = username;
+        this.password = password;
+        
+    }
+  
+   public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+  
 }
